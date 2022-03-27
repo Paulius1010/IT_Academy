7fa6ea2a56f81be8422597be4cee0a0a47be92b5
VitkunasPaulius <viitka@gmail.com>
2022-03-27 19:06:18 +0300

    first FE lesson

---
+++ HTML_CSS_JS/main.js
@@ -0,0 +1,12 @@
+// alert('test')
+// console.log("hello");
+// console.error("error");
+// console.warn("warning");
+
+
+// var banana = "banana";
+let age = 30;
+
+console.log(age);
+
+// let year = 1992;
